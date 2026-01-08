aea83af6741d01273f5e77f23ab5622b4cb7a26c
tushar pachare <tusharpachare78@gmail.com>
2026-01-08 17:09:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,156 +1,104 @@
-import bodyParser from "body-parser";
-import express from "express";
-import pg from "pg";
-import Razorpay from "razorpay";
-import env from "dotenv";
-import cors from "cors";
-// Connections from imports
+'use strict';
 
-const app = express();
-const port = 3000;
-env.config();
+Object.defineProperty(exports, "__esModule", { value: true });
 
-const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "amofoundation",
-    password: "123456",
-    port: 5432,
-});
+const picomatch = require('picomatch');
+const normalizePath = require('normalize-path');
 
-db.connect();
+/**
+ * @typedef {(testString: string) => boolean} AnymatchFn
+ * @typedef {string|RegExp|AnymatchFn} AnymatchPattern
+ * @typedef {AnymatchPattern|AnymatchPattern[]} AnymatchMatcher
+ */
+const BANG = '!';
+const DEFAULT_OPTIONS = {returnIndex: false};
+const arrify = (item) => Array.isArray(item) ? item : [item];
 
+/**
+ * @param {AnymatchPattern} matcher
+ * @param {object} options
+ * @returns {AnymatchFn}
+ */
+const createPattern = (matcher, options) => {
+  if (typeof matcher === 'function') {
+    return matcher;
+  }
+  if (typeof matcher === 'string') {
+    const glob = picomatch(matcher, options);
+    return (string) => matcher === string || glob(string);
+  }
+  if (matcher instanceof RegExp) {
+    return (string) => matcher.test(string);
+  }
+  return (string) => false;
+};
 
-var instance = new Razorpay({
-    key_id:process.env.KEY_ID,
-    key_secret: process.env.KEY_SECRET ,
-});
+/**
+ * @param {Array<Function>} patterns
+ * @param {Array<Function>} negPatterns
+ * @param {String|Array} args
+ * @param {Boolean} returnIndex
+ * @returns {boolean|number}
+ */
+const matchPatterns = (patterns, negPatterns, args, returnIndex) => {
+  const isList = Array.isArray(args);
+  const _path = isList ? args[0] : args;
+  if (!isList && typeof _path !== 'string') {
+    throw new TypeError('anymatch: second argument must be a string: got ' +
+      Object.prototype.toString.call(_path))
+  }
+  const path = normalizePath(_path, false);
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cors()); 
-
-// rendering pages
-
-app.get("/",(req,res)=>{
-    res.render("index.ejs");
-});
-
-app.get("/book",(req,res)=>{
-    res.render("book.ejs");
-});
-
-app.get("/about",(req,res)=>{
-    res.render("about.ejs");
-});
-
-app.get("/contact",(req,res)=>{
-    res.render("contact.ejs");
-});
-
-app.get("/orderRequest",(req,res)=>{
-    res.render("orderRequest.ejs");
-});
-
-
-
-// Newsletter Backend Integression
-
-app.post("/submit", async (req, res) => {
-    const { email } = req.body;
-
-    if (!email) {
-        return res.status(400).json({ message: "Email is required" });
+  for (let index = 0; index < negPatterns.length; index++) {
+    const nglob = negPatterns[index];
+    if (nglob(path)) {
+      return returnIndex ? -1 : false;
     }
+  }
 
-    try {
-        const result = await db.query(
-            "INSERT INTO subscriptions (email) VALUES ($1) RETURNING *;", 
-            [email]
-        );
-        console.log("✅ Inserted:", result.rows[0]);
-
-        res.status(200).json({ message: "Subscription successful!" });
-    } catch (error) {
-        console.error("❌ Error inserting data:", error);
-        if (error.code === "23505") {
-            res.status(409).json({ message: "Email already subscribed!" });
-        } else {
-            res.status(500).json({ message: "Error subscribing. Please try again later." });
-        }
+  const applied = isList && [path].concat(args.slice(1));
+  for (let index = 0; index < patterns.length; index++) {
+    const pattern = patterns[index];
+    if (isList ? pattern(...applied) : pattern(path)) {
+      return returnIndex ? index : true;
     }
-});
+  }
 
-// Order Section Data Taking 
+  return returnIndex ? -1 : false;
+};
 
-app.post("/order", async (req, res) => {
-    console.log(req.body); // Debugging Line
+/**
+ * @param {AnymatchMatcher} matchers
+ * @param {Array|string} testString
+ * @param {object} options
+ * @returns {boolean|number|Function}
+ */
+const anymatch = (matchers, testString, options = DEFAULT_OPTIONS) => {
+  if (matchers == null) {
+    throw new TypeError('anymatch: specify first argument');
+  }
+  const opts = typeof options === 'boolean' ? {returnIndex: options} : options;
+  const returnIndex = opts.returnIndex || false;
 
-    const { name, email, contact, address, pincode } = req.body;
+  // Early cache for matchers.
+  const mtchers = arrify(matchers);
+  const negatedGlobs = mtchers
+    .filter(item => typeof item === 'string' && item.charAt(0) === BANG)
+    .map(item => item.slice(1))
+    .map(item => picomatch(item, opts));
+  const patterns = mtchers
+    .filter(item => typeof item !== 'string' || (typeof item === 'string' && item.charAt(0) !== BANG))
+    .map(matcher => createPattern(matcher, opts));
 
-    try {
-        await db.query(
-            "INSERT INTO orders (full_name, email, contact_number, delivery_address, pincode, created_at) VALUES ($1, $2, $3, $4, $5, DEFAULT);",
-            [name, email, contact, address, pincode]
-        );
-        
-        console.log("Order inserted successfully");
-        res.redirect("/");
-    } catch (error) {
-        console.error("Database Insert Error:", error);
-        res.status(500).send("Database insert error");
+  if (testString == null) {
+    return (testString, ri = false) => {
+      const returnIndex = typeof ri === 'boolean' ? ri : false;
+      return matchPatterns(patterns, negatedGlobs, testString, returnIndex);
     }
-});
+  }
 
-// Razorpay Integration
-app.post("/proceedpay", async (req, res) => {
-    try {
-        const amount = req.body.amount;
+  return matchPatterns(patterns, negatedGlobs, testString, returnIndex);
+};
 
-        if (!amount) {
-            return res.status(400).json({ error: "Amount is required" });
-        }
-
-        const options = {
-            amount: amount * 100, // Convert to smallest currency unit
-            currency: "INR",
-            receipt: `receipt_${Date.now()}`,
-        };
-
-        const myOrder = await instance.orders.create(options);
-
-        res.status(201).json({
-            message: "Order created",
-            order: myOrder,
-            amount
-        });
-
-    } catch (error) {
-        console.error("Razorpay Order Error:", error);
-        res.status(500).json({ error: "Payment processing failed" });
-    }
-});
-
-// Handle Order Request Submission
-app.post("/submit-order", async (req, res) => {
-    const { fullname, email, contactno, requesttype, reason } = req.body;
-
-    try {
-        await db.query(
-            "INSERT INTO order_requests (full_name, email, contact_no, request_type, reason) VALUES ($1, $2, $3, $4, $5);",
-            [fullname, email, contactno, requesttype, reason]
-        );
-
-        res.json({ success: true, message: "Data saved successfully!" }); // FIX 2: Send Response
-
-    } catch (error) {
-        console.error("Database Insertion Error:", error);
-        res.status(500).json({ success: false, message: "Database error!" }); // FIX 2: Handle Errors
-    }
-});
-
-
-app.listen(port, () => {
-    console.log(`🚀 Server running on http://localhost:${port}`);
-});
+anymatch.default = anymatch;
+module.exports = anymatch;
